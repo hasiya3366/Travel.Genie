05a595de2385c22f1cc2ee2689fc4b38ab6e6902
Hasindu Indusara <hasinduindusara133@gmail.com>
2026-07-01 04:14:21 +0530

    Update TravelPlanRequest.java

--- Travel.Genie-master/TravelApp/src/main/java/com/example/TravelApp/model/TravelPlanRequest.java
+++ Travel.Genie-master/TravelApp/src/main/java/com/example/TravelApp/model/TravelPlanRequest.java
@@ -1,23 +1,3 @@
-// package com.example.TravelApp.model;
-
-// public class TravelPlanRequest {
-//     private String destination;
-//     private int durationDays;
-//     private String budget;
-//     private String season;
-//     private String companionType;
-
-//     public String getDestination() { return destination; }
-//     public void setDestination(String destination) { this.destination = destination; }
-//     public int getDurationDays() { return durationDays; }
-//     public void setDurationDays(int durationDays) { this.durationDays = durationDays; }
-//     public String getBudget() { return budget; }
-//     public void setBudget(String budget) { this.budget = budget; }
-//     public String getSeason() { return season; }
-//     public void setSeason(String season) { this.season = season; }
-//     public String getCompanionType() { return companionType; }
-//     public void setCompanionType(String companionType) { this.companionType = companionType; }
-// }
 package com.example.TravelApp.model;
 
 public class TravelPlanRequest {
@@ -26,16 +6,6 @@ public class TravelPlanRequest {
     private String budget;
     private String season;
     private String companionType;
-
-    public TravelPlanRequest() {}
-
-    public TravelPlanRequest(String destination, int durationDays, String budget, String season, String companionType) {
-        this.destination = destination;
-        this.durationDays = durationDays;
-        this.budget = budget;
-        this.season = season;
-        this.companionType = companionType;
-    }
 
     public String getDestination() { return destination; }
     public void setDestination(String destination) { this.destination = destination; }
@@ -48,3 +18,4 @@ public class TravelPlanRequest {
     public String getCompanionType() { return companionType; }
     public void setCompanionType(String companionType) { this.companionType = companionType; }
 }
+
