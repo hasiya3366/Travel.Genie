d8fc240f80b080c1b02576ff7ba79073d5b25825
Hasindu Indusara <hasinduindusara133@gmail.com>
2026-07-01 04:01:15 +0530

    Update ChatBotController.java

--- Travel.Genie-master/TravelApp/src/main/java/com/example/TravelApp/controller/ChatBotController.java
+++ Travel.Genie-master/TravelApp/src/main/java/com/example/TravelApp/controller/ChatBotController.java
@@ -1,3 +1,25 @@
+// package com.example.TravelApp.controller;
+
+// import com.example.TravelApp.model.TravelPlanRequest;
+// import com.example.TravelApp.service.ChatBotService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
+
+// @RestController
+// @RequestMapping("/api/chatbot")
+// @CrossOrigin(origins = "*")
+// public class ChatBotController {
+
+//     @Autowired
+//     private ChatBotService chatBotService;
+
+//     @PostMapping("/generate-itinerary")
+//     public ResponseEntity<String> generateItinerary(@RequestBody TravelPlanRequest request) {
+//         String itinerary = chatBotService.getSmartItinerary(request);
+//         return ResponseEntity.ok(itinerary);
+//     }
+// }
 package com.example.TravelApp.controller;
 
 import com.example.TravelApp.model.TravelPlanRequest;
@@ -8,7 +30,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/chatbot")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*") // Front-end එක කොහේ තිබ්බත් CORS Error එන එක වළක්වන්න
 public class ChatBotController {
 
     @Autowired
@@ -16,7 +38,11 @@ public class ChatBotController {
 
     @PostMapping("/generate-itinerary")
     public ResponseEntity<String> generateItinerary(@RequestBody TravelPlanRequest request) {
-        String itinerary = chatBotService.getSmartItinerary(request);
+        // Front-end එකෙන් එන JSON එක TravelPlanRequest object එකට auto-map වෙනවා.
+        // එතනින් destination (උදා: "Kandy") එක විතරක් අරන් ChatBotService එකට pass කරනවා.
+        String itinerary = chatBotService.getSmartItinerary(request.getDestination());
+        
+        // Gemini API එකෙන් ආපු පිළිතුර (Itinerary text එක) front-end එකට යවනවා.
         return ResponseEntity.ok(itinerary);
     }
 }
