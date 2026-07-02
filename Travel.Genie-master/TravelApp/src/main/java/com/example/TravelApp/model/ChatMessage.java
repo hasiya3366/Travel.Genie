a235e9bed6197f25caf2bc5321a15a532895f201
Hasindu Indusara <hasinduindusara133@gmail.com>
2026-07-02 22:20:54 +0530

    Refactor ChatMessage class and update package name

--- Travel.Genie-master/TravelApp/src/main/java/com/example/TravelApp/model/ChatMessage.java
+++ Travel.Genie-master/TravelApp/src/main/java/com/example/TravelApp/model/ChatMessage.java
@@ -1,22 +1,27 @@
-package com.travelgenie.model;
+package com.example.TravelApp.model;
 
 public class ChatMessage {
-    private String sessionId;
     private String sender;
     private String content;
     private MessageType type;
+    private String sessionId;
 
     public enum MessageType {
-        CHAT, JOIN, LEAVE
+        CHAT,
+        JOIN,
+        LEAVE
     }
 
-    // 💡 Getters and Setters (IntelliJ එකෙන් Alt+Insert ගහලා ඉබේම Generate කරගන්න මචං)
-    public String getSessionId() { return sessionId; }
-    public void setSessionId(String sessionId) { this.sessionId = sessionId; }
+    // --- GETTERS & SETTERS ---
     public String getSender() { return sender; }
     public void setSender(String sender) { this.sender = sender; }
+
     public String getContent() { return content; }
     public void setContent(String content) { this.content = content; }
+
     public MessageType getType() { return type; }
     public void setType(MessageType type) { this.type = type; }
+
+    public String getSessionId() { return sessionId; }
+    public void setSessionId(String sessionId) { this.sessionId = sessionId; }
 }
