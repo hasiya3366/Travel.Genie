7126f8e2c718a06913bd2335abb6b10b990a5c72
Hasindu Indusara <hasinduindusara133@gmail.com>
2026-07-02 22:16:21 +0530

    Add ComponentScan annotation to TravelAppApplication

--- Travel.Genie-master/TravelApp/src/main/java/com/example/TravelApp/TravelAppApplication.java
+++ Travel.Genie-master/TravelApp/src/main/java/com/example/TravelApp/TravelAppApplication.java
@@ -2,12 +2,12 @@ package com.example.TravelApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.TravelApp"})
 public class TravelAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TravelAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAppApplication.class, args);
+    }
 }
