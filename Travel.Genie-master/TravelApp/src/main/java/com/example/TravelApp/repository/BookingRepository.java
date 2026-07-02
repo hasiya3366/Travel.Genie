802b4bd6e530ae2dd5d0db3297deef56fcd19e7d
Hasindu Indusara <hasinduindusara133@gmail.com>
2026-07-03 02:20:14 +0530

    Change COALESCE to IFNULL in getTotalRevenue query

--- Travel.Genie-master/TravelApp/src/main/java/com/example/TravelApp/repository/BookingRepository.java
+++ Travel.Genie-master/TravelApp/src/main/java/com/example/TravelApp/repository/BookingRepository.java
@@ -31,6 +31,6 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
     
     List<Booking> findByUser(User user);
 
-    @Query(value = "SELECT COALESCE(SUM(b.price), 0.0) FROM booking b", nativeQuery = true)
-    Double getTotalRevenue();
+@Query(value = "SELECT IFNULL(SUM(price), 0.0) FROM booking", nativeQuery = true)
+Double getTotalRevenue();
 }
