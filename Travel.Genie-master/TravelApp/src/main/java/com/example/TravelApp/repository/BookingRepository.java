1f640c301adab2ccb51d9c932ab29c9ff8e0fb9e
Hasindu Indusara <hasinduindusara133@gmail.com>
2026-07-04 12:00:38 +0530

    Restore BookingRepository interface and methods
    Uncomment BookingRepository interface and its methods.

--- Travel.Genie-master/TravelApp/src/main/java/com/example/TravelApp/repository/BookingRepository.java
+++ Travel.Genie-master/TravelApp/src/main/java/com/example/TravelApp/repository/BookingRepository.java
@@ -1,21 +1,3 @@
-// package com.example.TravelApp.repository;
-
-// import com.example.TravelApp.model.Booking;
-// import com.example.TravelApp.model.User;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.data.jpa.repository.Query;
-// import org.springframework.stereotype.Repository;
-
-// import java.util.List;
-
-// @Repository
-// public interface BookingRepository extends JpaRepository<Booking, Long> {
-    
-//     List<Booking> findByUser(User user);
-
-//     @Query(value = "SELECT COALESCE(SUM(price), 0.0) FROM booking", nativeQuery = true)
-//     double getTotalRevenue();
-// }
 package com.example.TravelApp.repository;
 
 import com.example.TravelApp.model.Booking;
