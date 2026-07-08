cdc5d485c975d98d2b052b8ae2c4ad68be162d9e
kesarindu <manujakrathnayake@gmail.com>
2026-07-08 16:00:10 +0530

    Update ChatBotController.java

--- Travel.Genie-master/TravelApp/src/main/java/com/example/TravelApp/controller/ChatBotController.java
+++ Travel.Genie-master/TravelApp/src/main/java/com/example/TravelApp/controller/ChatBotController.java
@@ -1,22 +1,22 @@
-package com.example.TravelApp.controller;
+// package com.example.TravelApp.controller;
 
-import com.example.TravelApp.model.TravelPlanRequest;
-import com.example.TravelApp.service.ChatBotService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+// import com.example.TravelApp.model.TravelPlanRequest;
+// import com.example.TravelApp.service.ChatBotService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/chatbot")
-@CrossOrigin(origins = "*")
-public class ChatBotController {
+// @RestController
+// @RequestMapping("/api/chatbot")
+// @CrossOrigin(origins = "*")
+// public class ChatBotController {
 
-    @Autowired
-    private ChatBotService chatBotService;
+//     @Autowired
+//     private ChatBotService chatBotService;
 
-    @PostMapping("/generate-itinerary")
-    public ResponseEntity<String> generateItinerary(@RequestBody TravelPlanRequest request) {
-        String itinerary = chatBotService.getSmartItinerary(request);
-        return ResponseEntity.ok(itinerary);
-    }
-}
+//     @PostMapping("/generate-itinerary")
+//     public ResponseEntity<String> generateItinerary(@RequestBody TravelPlanRequest request) {
+//         String itinerary = chatBotService.getSmartItinerary(request);
+//         return ResponseEntity.ok(itinerary);
+//     }
+// }
